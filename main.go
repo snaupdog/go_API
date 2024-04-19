9d763defa7cde07881427de2cd5078ca21b4ed01
snau <nihal.satish@gmail.com>
2024-04-19 15:55:06 +0530

    uhuho

--- main.go
+++ main.go
@@ -1,30 +1,56 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
-    "net/http"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-type Book struct {
-    Title  string `json:"title"`
-    Author string `json:"author"`
+type FitnessData struct {
+	Date          string `json:"date"`
+	HeartTracker  []int  `json:"heart_tracker"`
+	CaloriesBurned int    `json:"calories_burned"`
+	StepCount     int    `json:"step_count"`
 }
 
-var books = []Book{
-    {Title: "Book 1", Author: "nilesh 1"},
-    {Title: "Book 2", Author: "Author 2"},
-    {Title: "Book 3", Author: "Author 3"},
+var fitnessData = []FitnessData{
+	{
+		Date:           "2024-04-19",
+		HeartTracker:   []int{78, 80, 76, 82},
+		CaloriesBurned: 350,
+		StepCount:      8000,
+	},
+	{
+		Date:           "2024-04-18",
+		HeartTracker:   []int{78, 80, 76, 82},
+		CaloriesBurned: 400,
+		StepCount:      10000,
+	},
 }
 
 func main() {
-    router := gin.Default()
-    router.Use(cors.Default())
+	router := gin.Default()
+	router.Use(cors.Default())
 
-    // Endpoint to retrieve list of books
-    router.GET("/books", func(c *gin.Context) {
-        c.JSON(http.StatusOK, books)
-    })
+	router.StaticFile("/", "./index.html")
 
-    router.Run(":8080")
+	// Endpoint to retrieve fitness data
+	router.GET("/data", func(c *gin.Context) {
+		c.JSON(http.StatusOK, fitnessData)
+	})
+
+	// Endpoint to add fitness data
+	router.POST("/addData", func(c *gin.Context) {
+		var newData FitnessData
+		if err := c.BindJSON(&newData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		// Add the new data to the slice
+		fitnessData = append(fitnessData, newData)
+		c.Status(http.StatusOK)
+	})
+
+	router.Run(":8080")
 }

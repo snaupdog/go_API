c7dd76d1e97938cdf472a24cc8f2ff00a9637978
snau <nihal.satish@gmail.com>
2024-04-23 11:15:06 +0530

    put and get request fixed

--- main.go
+++ main.go
@@ -2,55 +2,55 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+"github.com/gin-contrib/cors"
 )
 
+
+
 type FitnessData struct {
+
+	ID     int    `json:"ID"`
 	Date          string `json:"date"`
-	HeartTracker  []int  `json:"heart_tracker"`
-	CaloriesBurned int    `json:"calories_burned"`
+	HeartRate     int    `json:"heart_rate"`
 	StepCount     int    `json:"step_count"`
 }
 
-var fitnessData = []FitnessData{
-	{
-		Date:           "2024-04-19",
-		HeartTracker:   []int{78, 80, 76, 82},
-		CaloriesBurned: 350,
-		StepCount:      8000,
-	},
-	{
-		Date:           "2024-04-18",
-		HeartTracker:   []int{78, 80, 76, 82},
-		CaloriesBurned: 400,
-		StepCount:      10000,
-	},
+
+var fitnessData []FitnessData
+
+func getData(c *gin.Context){
+	c.JSON(http.StatusOK, fitnessData)
 }
 
-func main() {
-	router := gin.Default()
-	router.Use(cors.Default())
 
-	router.StaticFile("/", "./index.html")
+func addData(c *gin.Context){
 
-	// Endpoint to retrieve fitness data
-	router.GET("/data", func(c *gin.Context) {
-		c.JSON(http.StatusOK, fitnessData)
-	})
-
-	// Endpoint to add fitness data
-	router.POST("/addData", func(c *gin.Context) {
 		var newData FitnessData
 		if err := c.BindJSON(&newData); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 		// Add the new data to the slice
+    newData.ID = len(fitnessData)+1
+    println(newData.ID,newData.Date)
 		fitnessData = append(fitnessData, newData)
-		c.Status(http.StatusOK)
-	})
+    c.JSON(http.StatusCreated,newData)
+}
+
+
+
+func main() {
+    gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+ router.Use(cors.Default())
+	// router.Use(cors.Default())
+
+
+    router.POST("/data",addData)
+    router.GET("/data",getData)
+
+	router.StaticFile("/", "./index.html")
 
 	router.Run(":8080")
 }
